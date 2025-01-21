0a81b8cc64487f0638104eb82fa504dfa04f1020
Guillaume Vitry <guy.vitry@aqua-superpower.com>
2025-01-21 07:59:49 +0000

    puppy can do BigBark

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Guius/puppy
 
 go 1.21.3
+
+require github.com/Guius/Dog v0.0.0-20250121075658-5e437f49b362 // indirect
--- puppy.go
+++ puppy.go
@@ -1,6 +1,17 @@
 package puppy
 
+import (
+	dog "github.com/Guius/Dog"
+)
 
 func Bark() string {
 	return "Bark"
+}
+
+func Barks() string {
+	return "Bark Bark Bark"
+}
+
+func BigBark() string {
+	return dog.WhenGrowUp(Bark())
 }
